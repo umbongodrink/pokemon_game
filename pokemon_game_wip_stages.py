9986b65af2eaa75d35264d451b3cc27d32c57284
Kip Thompson-Born <kipthompsonborn@hotmail.com>
2024-12-09 20:25:44 +0000

    added the privisional answer

--- pokemon_game_wip_stages.py
+++ pokemon_game_wip_stages.py
@@ -1,23 +1,21 @@
 import requests
 import json
 import random
-
-# pprint is a module that will help us print the data in a more readable format when debugging the battle dictionary
 import pprint
 
-
-# Create a function that will download the characteristics of each Pokemon
 def calculate_characteristics(name_of_pokemon):
     url = f'https://pokeapi.co/api/v2/pokemon/{name_of_pokemon}/'
     response = requests.get(url)
     pokemon_data = response.json()
+
     ability = pokemon_data['abilities'][0]['ability']['name']
     height_formatted = pokemon_data['height'] / 10
     weight_formatted = pokemon_data['weight'] / 10
-    winning_factor = round(height_formatted / weight_formatted, 3)
+
+    winning_factor = height_formatted / weight_formatted
+
     return name_of_pokemon, ability, height_formatted, weight_formatted, winning_factor
 
-# Create a function that will add the Pokemon to the battle dictionary
 def add_pokemon_to_battle_dictionary(pokemon_characteristics, battle_dictionary):
     battle_dictionary[pokemon_characteristics[0]] = {
         "ability": pokemon_characteristics[1],
@@ -25,7 +23,6 @@ def add_pokemon_to_battle_dictionary(pokemon_characteristics, battle_dictionary)
         "weight_formatted": pokemon_characteristics[3],
         "winning_factor": pokemon_characteristics[4]
     }
-
 
 # Get the list of Pokemon from the API
 url = 'https://pokeapi.co/api/v2/pokemon/'
@@ -44,47 +41,66 @@ length_of_pokemon_list = len(list_of_names)
 random_pokemon_number = random.randint(0, length_of_pokemon_list - 1)
 random_pokemon = list_of_names[random_pokemon_number]
 cpu_pokemon_choice = random_pokemon
-print(f"\nYou will fight against: {cpu_pokemon_choice.capitalize()}")
+print(f"You will fight against: {cpu_pokemon_choice.capitalize()}")
 
 # This code prints out a list of pokemon names with capital letters for the user to choose from
 for i in range(len(list_of_names)):
     list_of_names[i] = list_of_names[i].capitalize()
-print(f"\nYou will now have to select your Pokemon to fight from this list:\n{list_of_names}")
+print(f"You will now have to select your pokemon to fight from this list:\n{list_of_names}")
 
 # Ask the user to choose a pokemon
-print(f'\nEnter your Pokemon:')
+print('Enter your pokemon:')
 # Get the user's choice
 choice = input().lower()
 
 # Logic to check if the user's choice is in the list of names
 while choice.capitalize() not in list_of_names:
-    print("Please enter a valid Pokemon name:")
+    print("Please enter a valid pokemon name:")
     choice = input().lower()
 
 # Logic to check if the user's choice is the same as the CPU's choice. If it is, ask the user to choose again
 while choice.capitalize() == cpu_pokemon_choice.capitalize():
-    print("You can't choose the same Pokemon as the CPU. Please choose again:")
+    print("You can't choose the same pokemon as the CPU. Please choose again:")
     choice = input().lower()
 
 # Display the who the battle is between
-print(f"\nThe fight will be between: {cpu_pokemon_choice.capitalize()} and {choice.capitalize()}!\n"
-      f"Let battle commence!\n")
+print(f"The fight will be between: {cpu_pokemon_choice.capitalize()} and {choice.capitalize()}\n")
 
 # Create an empty dict to hold the battle data
 pokemon_battle_dict = {}
+
+# Create a function that will download the characteristics of each pokemon
+# def calculate_characteristics(name_of_pokemon):
+#     url = f'https://pokeapi.co/api/v2/pokemon/{name_of_pokemon}/'
+#     response = requests.get(url)
+#     pokemon_data = response.json()
+#
+#     ability = pokemon_data['abilities'][0]['ability']['name']
+#     height_formatted = pokemon_data['height'] / 10
+#     weight_formatted = pokemon_data['weight'] / 10
+#
+#     winning_factor = height_formatted / weight_formatted
+#
+#     return name_of_pokemon, ability, height_formatted, weight_formatted, winning_factor
 
 # Get the characteristics of the CPU pokemon and the user's choice
 cpu_pokemon_characteristics = calculate_characteristics(cpu_pokemon_choice)
 choice_characteristics = calculate_characteristics(choice)
 
-# Add the characteristics to the battle dictionary
+# def add_pokemon_to_battle_dictionary(pokemon_characteristics, battle_dictionary):
+#     battle_dictionary[pokemon_characteristics[0]] = {
+#         "ability": pokemon_characteristics[1],
+#         "height_formatted": pokemon_characteristics[2],
+#         "weight_formatted": pokemon_characteristics[3],
+#         "winning_factor": pokemon_characteristics[4]
+#     }
+
 add_pokemon_to_battle_dictionary(cpu_pokemon_characteristics, pokemon_battle_dict)
 add_pokemon_to_battle_dictionary(choice_characteristics, pokemon_battle_dict)
 
-# Print the Pokemon battle dictionary so you can see the data. This is for debugging purposes, and can uncommented if
-# needed.
-# pprint.pprint(pokemon_battle_dict)
-# print("\n")
+
+pprint.pprint(pokemon_battle_dict)
+print("\n")
 
 # Print the battle data for each pokemon
 for pokemon, data in pokemon_battle_dict.items():
@@ -94,9 +110,11 @@ for pokemon, data in pokemon_battle_dict.items():
     print(f"Height: {data['height_formatted']}m")
     print(f"Winning factor: {data['winning_factor']}\n")
 
+#print("\n")
+
 # Determine the winner of the battle
 for name, values in pokemon_battle_dict.items():
-    print(f"The Pokemon {name.capitalize()} has a winning factor of {values['winning_factor']}")
+    print(f"The Pokemon {name.capitalize()} has the winning factor {values['winning_factor']}")
 
 print("\n")
 
@@ -106,3 +124,10 @@ elif pokemon_battle_dict[cpu_pokemon_choice]['winning_factor'] < pokemon_battle_
     print(f"{choice.capitalize()} wins! You win!")
 else:
     print("It's a draw!")
+
+
+
+
+
+
+
